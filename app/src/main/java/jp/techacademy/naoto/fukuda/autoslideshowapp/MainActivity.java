9548ffbf1a1110c470dccecbb9878535fe406adf
naotofukuda <nfuku30@gmail.com>
2017-06-11 11:06:13 +0900

    進む、戻る、再生停止ボタン機能追加（タイマー除く）

--- app/src/main/java/jp/techacademy/naoto/fukuda/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/naoto/fukuda/autoslideshowapp/MainActivity.java
@@ -18,11 +18,12 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button mForwardButton;
-    Button mStartButton;
+    Button mStartPauseButton;
     Button mBackButton;
     Cursor cursor; //カーソルのインスタンス:メンバ変数として指定(→クラス全体で使用可）
     ImageView imageView;
-    
+    boolean autoflag = true; //autoflag = ture 初期値、再生ON。false 停止ON。
+
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
 
@@ -32,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mForwardButton = (Button) findViewById(R.id.forward_button);
-        mStartButton = (Button) findViewById(R.id.start_button);
+        mStartPauseButton = (Button) findViewById(R.id.start_button);
         mBackButton = (Button) findViewById(R.id.back_button);
         imageView = (ImageView) findViewById(R.id.imageView1);
 
@@ -51,30 +52,41 @@ public class MainActivity extends AppCompatActivity {
             getContentsInfo();
         }
 
+        mStartPauseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(autoflag){ //再生ON
 
+                    getContentsInfoForward();
+                    mStartPauseButton.setText("停止");
+                    autoflag =! autoflag;
 
-    mStartButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            getContentsInfo();
+                    }else { //再生OFF = 停止ON
 
-        }
-    });
+                    getContentsInfoBack();
+                    mStartPauseButton.setText("再生");
+                    autoflag =! autoflag;
 
-    mForwardButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            getContentsInfoForward();
+                    }
+
+                }
+
+        });
+
+        mForwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getContentsInfoForward();
 
             }
         });
 
-    mBackButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-        }
-    });
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getContentsInfoBack();
+            }
+        });
 
     }
 
@@ -101,77 +113,49 @@ public class MainActivity extends AppCompatActivity {
                 null, // フィルタ条件(null = フィルタなし)
                 null, // フィルタ用パラメータ
                 null // ソート (null ソートなし)
+                //メンバ変数Cursor cursorに値は保存される。
         );
 
         if (cursor.moveToFirst()) {
+
+            cursor.moveToFirst();
+            mStartPauseButton.setText("再生");
+            setImageView();
+            // リファクタリング。複数メソッドで共通する動作は、メソッドにしてしまう。setImageView();
             // indexからIDを取得し、そのIDから画像のURIを取得する
-            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            Long id = cursor.getLong(fieldIndex);
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-            imageView.setImageURI(imageUri);
-            Log.d("ANDROID", "URI : " + imageUri.toString());
-
-        }cursor.close();
+        }
     }
 
-
-    private void getContentsInfoForward(){
-
-       /* // 画像の情報を取得する
-        ContentResolver resolver = getContentResolver();
-        cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                null, // 項目(null = 全項目)
-                null, // フィルタ条件(null = フィルタなし)
-                null, // フィルタ用パラメータ
-                null // ソート (null ソートなし)
-        ); */
+    private void getContentsInfoForward() {
 
         if (cursor.moveToNext()) {
             // indexからIDを取得し、そのIDから画像のURIを取得する
-          /*  int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            Long id = cursor.getLong(fieldIndex);
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-            */
-
-            imageView.setImageURI(imageUri);
-            Log.d("ANDROID", "URI : " + imageUri.toString());
-        }else {
+            setImageView();
+        } else {
             cursor.moveToFirst();
-           /* int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-            Long id = cursor.getLong(fieldIndex);
-            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-             */
-            imageView.setImageURI(imageUri);
-            Log.d("ANDROID", "URI : " + imageUri.toString());
+            setImageView();
         }
-        cursor.close();
+
     }
 
-    private void getContentsInfoBack(){
+    private void getContentsInfoBack() {
 
 
         if (cursor.moveToPrevious()) {
-       /* // indexからIDを取得し、そのIDから画像のURIを取得する
+            setImageView();
+        } else {
+            cursor.moveToLast();
+            setImageView();
+        }
+    }
+
+    private void setImageView() { //Refactaringした。
         int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
         Long id = cursor.getLong(fieldIndex);
         Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-        */
 
-        imageView.setImageURI(imageUri);
         Log.d("ANDROID", "URI : " + imageUri.toString());
-    }else {
-        cursor.moveToLast();
-       /* int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-        Long id = cursor.getLong(fieldIndex);
-        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-         */
-        imageView = (ImageView) findViewById(R.id.imageView1);
         imageView.setImageURI(imageUri);
-        Log.d("ANDROID", "URI : " + imageUri.toString());
-    }
-        cursor.close();
     }
 
 }
